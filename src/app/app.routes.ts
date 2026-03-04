163e16221543a9b983bf0b07dba933fabfc9f6ef
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2026-03-03 21:09:12 -0300

    Configure project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CreateGameComponent } from './pages/create-game/create-game/create-game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create-game',
+        component: CreateGameComponent
+    }
+];
