f73cd31db5c85ebfc050fb80a2f8cb4194fb39b3
Rafael <rafael.ferreira@inventsoftware.com.br>
2026-03-05 19:21:06 -0300

    Bs

--- src/app/components/conteudo-principal/conteudo-principal.component.ts
+++ src/app/components/conteudo-principal/conteudo-principal.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-conteudo-principal',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './conteudo-principal.component.html',
-  styleUrl: './conteudo-principal.component.css'
+  styleUrl: './conteudo-principal.component.css',
+  standalone: true
 })
 export class ConteudoPrincipalComponent {
 
